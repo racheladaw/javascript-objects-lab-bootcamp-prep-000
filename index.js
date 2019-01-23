12cc9fb539761820d9b80e638100e39789c5d5ad
Rachel Williams <williamsrachelada@gmail.com>
2019-01-23 22:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object,{[key]: value})
   return newObj
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
